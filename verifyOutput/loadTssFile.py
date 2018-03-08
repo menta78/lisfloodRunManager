8d94a4d20c3eb264b5aff450dcde28ddb8f002a8
Lorenzo Mentaschi <mentalo@D01RI1402967.ies.jrc.it>
2018-03-08 10:54:18 +0100

    functions to load output tss file and some test plots

---
+++ verifyOutput/loadTssFile.py
@@ -0,0 +1,75 @@
+import os, re
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import numpy as np
+
+
+
+"""
+example of file path:
+/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/historical/IPSL-INERIS-WRF331F/notWaterUse/disWin_IPSL-INERIS-WRF331F_historical_1981010100.tss
+which startDate is 19810101
+"""
+def loadTssFile(filePath, startDate=datetime(1981, 01, 01), timeDelta = relativedelta(days=1), selectStatIds = None):
+  fl = open(filePath)
+  fl.readline()
+  ln = fl.readline().strip(' \n\r\t')
+  nstat = int(ln)
+  fl.readline()
+  statIds = []
+  for i in range(nstat-1):
+    ln = fl.readline().strip(' \n\r\t')
+    statIds.append(int(ln))
+  
+  tms = []
+  dis = []
+  actDate = startDate
+  for ln in fl:
+    ln = ln.strip(' \n\r\t')
+    vls = ln.split()
+    timeStep = int(vls[0])
+    disActDate = [float(v) for v in vls[1:]]
+    actDate = startDate + (timeStep - 1)*timeDelta
+    tms.append(actDate)
+    dis.append(disActDate)
+
+  tms = np.array(tms)
+  statIds = np.array(statIds)
+  dis = np.array(dis)
+
+  statIndx = np.argsort(statIds)
+  statIds = statIds[statIndx]
+  dis = dis[:, statIndx]
+
+  if not selectStatIds is None:
+    selectStatIds = np.array(selectStatIds)
+    idx = np.searchsorted(statIds, selectStatIds)
+    dis = dis[:, idx]
+    statIds = selectStatIds
+
+  dis[dis > 1e20] = 0
+
+  return tms, statIds, dis
+
+
+
+def loadTssFromDir(dirPath, selectStatIds, startDate=datetime(1981, 01, 01), timeDelta = relativedelta(days=1)):
+  print('loading from ' + dirPath)
+  fls = [f for f in os.listdir(dirPath) if re.match('disWin(.*)\.tss', f)]
+  fls.sort()
+  tms, dis = None, None
+  actDate = startDate - timeDelta
+  for f in fls:
+    print('  loading ' + f)
+    fpth = os.path.join(dirPath, f)
+    tmsAct, statIds, disAct = loadTssFile(fpth, startDate=startDate, timeDelta=timeDelta, selectStatIds=selectStatIds)
+    indx = np.where(tmsAct > actDate)[0]
+    tmsAct = tmsAct[indx]
+    disAct = disAct[indx, :]
+    tms = tmsAct if tms is None else np.concatenate([tms, tmsAct], 0)
+    dis = disAct if dis is None else np.concatenate([dis, disAct], 0)
+    actDate = np.max(tmsAct)
+  return tms, statIds, dis
+    
+    
+
---
+++ verifyOutput/testPlot.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from matplotlib import pyplot as plt
+
+import loadTssFile
+
+
+
+def plotSingle(tms, dis):
+  p = plt.plot(tms, dis[:,0])
+  plt.grid('on')
+  return p
+
+
+
+def testPlotMax():
+  flpth = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/historical/IPSL-INERIS-WRF331F/notWaterUse/disWin_IPSL-INERIS-WRF331F_historical_1982010100.tss'
+
+  fldirHist = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/historical/IPSL-INERIS-WRF331F/notWaterUse/'
+  fldirRcp85 = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/rcp85/IPSL-INERIS-WRF331F/notWaterUse/'
+  fldirRcp45 = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/rcp45/IPSL-INERIS-WRF331F/notWaterUse/'
+  histStartDate = datetime(1981, 01, 01)
+  rcpStartDate = datetime(2011, 01, 01)
+
+  tms, statIds, dis = loadTssFile.loadTssFile(flpth)
+  mxindx = np.where(dis[-1,:] == max(dis[-1,:]))
+  selectStatIds = statIds[mxindx]
+  tmsHist, statIds, disHist = loadTssFile.loadTssFromDir(fldirHist, startDate=histStartDate, selectStatIds=selectStatIds)
+  plotSingle(tmsHist, disHist)
+  tmsRcp85, statIds, disRcp85 = loadTssFile.loadTssFromDir(fldirRcp85, startDate=rcpStartDate, selectStatIds=selectStatIds)
+  plotSingle(tmsRcp85, disRcp85)
+  tmsRcp45, statIds, disRcp45 = loadTssFile.loadTssFromDir(fldirRcp45, startDate=rcpStartDate, selectStatIds=selectStatIds)
+  plotSingle(tmsRcp45, disRcp45)
+
+
+
+def testPlotStat(statId = 210):
+  fldirHist = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/historical/IPSL-INERIS-WRF331F/notWaterUse/'
+  fldirRcp85 = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/rcp85/IPSL-INERIS-WRF331F/notWaterUse/'
+  fldirRcp45 = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/rcp45/IPSL-INERIS-WRF331F/notWaterUse/'
+  msrsFl = '/STORAGE/src1/git/lisfloodRunManager/verifyOutput/testdata/disWin_measuresGauges.tss'
+  histStartDate = datetime(1981, 01, 01)
+  rcpStartDate = datetime(2011, 01, 01)
+  msrsStartDate = datetime(1991, 01, 01)
+
+  selectStatIds = [statId]
+  tmsHist, statIds, disHist = loadTssFile.loadTssFromDir(fldirHist, startDate=histStartDate, selectStatIds=selectStatIds)
+  tmsRcp85, statIds, disRcp85 = loadTssFile.loadTssFromDir(fldirRcp85, startDate=rcpStartDate, selectStatIds=selectStatIds)
+  tmsRcp45, statIds, disRcp45 = loadTssFile.loadTssFromDir(fldirRcp45, startDate=rcpStartDate, selectStatIds=selectStatIds)
+  tmsMsrs, statIds, disMsrs = loadTssFile.loadTssFile(msrsFl, startDate=msrsStartDate, selectStatIds=selectStatIds)
+  plotSingle(tmsHist, disHist)
+  plotSingle(tmsRcp85, disRcp85)
+  plotSingle(tmsRcp45, disRcp45)
+  plotSingle(tmsMsrs, disMsrs)
+  plt.plot(tmsMsrs, disMsrs)
+
+
+
+
+def testSalzburgScatters():
+  statId = 210
+  fldirHist = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/historical/IPSL-INERIS-WRF331F/notWaterUse/'
+
+  
+
+
+
