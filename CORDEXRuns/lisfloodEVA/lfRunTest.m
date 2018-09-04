a39a2d95194ac84a9232ecf5aba94f2e566b33b6
Lorenzo Mentaschi <lorenzo.mentaschi@ec.europa.eu>
2018-09-04 10:54:03 +0200

    EVA: preparing test for hpc

--- CORDEXRuns/lisfloodEVA/lfRunTest.m
+++ CORDEXRuns/lisfloodEVA/lfRunTest.m
@@ -1,8 +1,11 @@
+eosDir = 'DATA/JEODPP/eos/projects';
+eosDir = '/ADAPTATION/mentalo/JEODPP/EOS/projects';
+
 addpath('/STORAGE/src1/git/tsEva/');
 model = 'CLMcom-CCLM4-8-17_BC_ICHEC-EC-EARTH';
 scenario = 'rcp85';
 %scenario = 'rcp45';
 wuChanging = false;
 outDir = './test';
-ncRootDir = '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/';
+ncRootDir = fullfile(eosDir, '/DATA/JEODPP/eos/projects/CRITECH/ADAPTATION/ClimateRuns/LisfloodEuroCordex/');
 lfEvaModel(scenario, model, wuChanging, outDir, 'ncRootDir', ncRootDir);
