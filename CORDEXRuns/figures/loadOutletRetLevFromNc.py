ecec4a223c2f4ee09acaa321d0a55cc2e13f0723
Lorenzo Mentaschi <lorenzo.mentaschi@ec.europa.eu>
2018-11-07 16:05:54 +0100

    some improvement in the plots

---
+++ CORDEXRuns/figures/loadOutletRetLevFromNc.py
@@ -0,0 +1,40 @@
+import netCDF4
+import numpy as np
+import os
+
+
+def loadOutletRetLevFromNc(ncEvaMapPth, returnPeriod, ncOutletPth=''):
+  if ncOutletPth == '':
+    ncOutletPth = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'outlets.nc')
+
+  outletDs = netCDF4.Dataset(ncOutletPth)
+  outlets = outletDs.variables['outlets'][:]
+  oirow, oicol = np.where(np.logical_not(outlets.mask))
+
+  disDs = netCDF4.Dataset(ncEvaMapPth)
+  year = disDs.variables['year'][:]
+  retper = disDs.variables['return_period'][:]
+  rpIndx = np.where(retper == returnPeriod)[0][0]
+  rl = disDs.variables['rl']
+
+  outPtIds = []
+  timeSrs = []
+  npts = len(oirow)
+  for irow, icol in zip(oirow, oicol):
+    
+    outPtIdAct = outlets[irow, icol]
+    outPtIds.append(outPtIdAct)
+    timeSrsAct = rl[rpIndx, :, icol, irow]
+    timeSrs.append(timeSrsAct)
+  outPtIds = np.array(outPtIds)
+ #timeSrsWithNan = np.array(timeSrs).transpose()
+ #timeSrs = timeSrsWithNan
+ #timeSrs[np.isnan(timeSrs)] = 1e31
+
+  outPtIdsFinal = np.arange(max(outPtIds)) + 1
+  timeSrs = np.array(timeSrs)
+  timeSrsFinal = np.ones([outPtIdsFinal.shape[0], timeSrs.shape[1]])*np.nan
+  outPtIndxs = outPtIds - 1
+  timeSrsFinal[outPtIndxs, :] = timeSrs
+  return year, outPtIdsFinal, timeSrsFinal
+
--- CORDEXRuns/figures/plotProjectedChange.py
+++ CORDEXRuns/figures/plotProjectedChange.py
@@ -145,8 +145,6 @@ KNMI-RACMO22E-ICHEC-EC-EARTH_BC
 
   outputPngFile = '100yrlChange_ensembles.png'
 
-  nrow = 4
-  ncol = 3
   fig = plt.figure(figsize=(10, 4))
   gs = gridspec.GridSpec(1, 3, width_ratios=[1,1,.1])
   lon, lat = [], []
@@ -231,6 +229,63 @@ KNMI-RACMO22E-ICHEC-EC-EARTH_BC
   plotSingleModelVar(ax, 'Ensemble ' + scenario + ', warming lev. ' + str(warmingLev) + '$^\circ$', lon, lat, projVar, mp, bold=True)
 
   ax = plt.subplot(gs[:, 3])
+ #ax.get_xaxis().set_visible(False)
+ #ax.get_yaxis().set_visible(False)
+  cb = plt.colorbar(pcl, cax=ax)
+  cb.set_label(label='100-year event change (%)', fontsize=12)
+  plt.tight_layout()
+
+  fig.savefig(outputPngFile, dpi=300)
+
+
+
+
+def plotEnsemblesWarmingLevel(warmingLev=2.0, rootDir='/ClimateRun4/multi-hazard/eva/'):
+  scenarios = ['rcp85', 'rcp45']
+  models = """
+IPSL-INERIS-WRF331F_BC
+SMHI-RCA4_BC_CNRM-CERFACS-CNRM-CM5
+SMHI-RCA4_BC_ICHEC-EC-EARTH
+SMHI-RCA4_BC_IPSL-IPSL-CM5A-MR
+SMHI-RCA4_BC_MOHC-HadGEM2-ES
+SMHI-RCA4_BC_MPI-M-MPI-ESM-LR
+CLMcom-CCLM4-8-17_BC_CNRM-CERFACS-CNRM-CM5
+CLMcom-CCLM4-8-17_BC_ICHEC-EC-EARTH
+CLMcom-CCLM4-8-17_BC_MPI-M-MPI-ESM-LR
+DMI-HIRHAM5-ICHEC-EC-EARTH_BC
+KNMI-RACMO22E-ICHEC-EC-EARTH_BC
+"""
+  models = models.split()
+
+  outputPngFile = '100yrlChange_ensembles_wl.png'
+
+  fig = plt.figure(figsize=(10, 4))
+  gs = gridspec.GridSpec(1, 3, width_ratios=[1,1,.1])
+  lon, lat = [], []
+  bslnlist, projlist = [], []
+  mp = None
+  for scenario, iscen in zip(scenarios, range(len(scenarios))):
+    wlYear = getWarmingLevels(scenario, warmingLev)
+    for mdl, imdl in zip(models, range(1, len(models) + 1)):
+      print('getting model ' + mdl)
+    
+      wrmYear = wlYear[mdl]
+      wrmYear = int(round(wrmYear/5.)*5.)
+      
+      ncFlNm = '_'.join(['projection_dis', scenario, mdl, 'wuChang', 'statistics.nc'])
+      ncFlPth = os.path.join(rootDir, ncFlNm)
+      mdlname = mdl.replace('BC_', '').replace('_BC', '')
+      lon, lat, bsln, proj, pcl, mp = plotSingleModel(None, ncFlPth, mdlname, mp=mp, projYear=wrmYear)
+      bslnlist.append(bsln)
+      projlist.append(proj)
+
+    ax = plt.subplot(gs[iscen])
+    bsln = np.nanmean(np.array(bslnlist), 0)
+    proj = np.nanmean(np.array(projlist), 0)
+    projVar = (proj - bsln)/bsln*100
+    plotSingleModelVar(ax, 'Ensemble ' + scenario + '\n wrm. lev. ' + str(warmingLev), lon, lat, projVar, mp, bold=True, titleFontSize=10)
+
+  ax = plt.subplot(gs[2])
  #ax.get_xaxis().set_visible(False)
  #ax.get_yaxis().set_visible(False)
   cb = plt.colorbar(pcl, cax=ax)
